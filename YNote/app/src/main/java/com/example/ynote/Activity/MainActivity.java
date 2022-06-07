5daa1baa95ea72b804b2531c891c63cd890d7a6b
神里 <xinhuo2001@163.com>
2022-06-07 21:34:32 +0800

    备忘录初具雏形

---
+++ YNote/app/src/main/java/com/example/ynote/Activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ynote.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ynote.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_test);
+    }
+}
